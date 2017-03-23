bf54e6cca445db6520c1b3c2abececf1fd4c3c8d
venkatsr16 <venkat5494@gmail.com>
2017-03-23 21:21:35 +0530

    Problem statement
    a program to print the result of the following expressions provided the
    integer variable a is
    20 and b is 10.
    int b= a-- - --a;
    int c=a--;
    int d=a>>2;
    int e=a&b;

---
+++ ass2.java
@@ -0,0 +1,17 @@
+class ass2
+{
+public static void main(String[] args)
+{
+int a , b , c , d , e;
+a = 20;
+b = 10;
+b= a-- - --a;
+System.out.println("b = " + b);
+c=a--;
+System.out.println("c = " + c);
+d=a>>2;
+System.out.println("d = " + d);
+e=a&b;
+System.out.println("e = " + e);
+}
+}
